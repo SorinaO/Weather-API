32238b41600ea695d1824f617177c19ce0b2f4a0
sorina <oantas@yahoo.com>
2025-01-09 01:35:48 +0000

    Add URL endpoint for yearly and all data

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 # Strip extra spaces from column names
 stations.columns = stations.columns.str.strip()
 
+
 # Select only the 'STAID' and 'STANAME' columns
 station = stations[["STAID", "STANAME"]]
+
 
 # Domain name
 @app.route("/")
 def home():
     return render_template("home.html", data=station.to_html())
+
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
@@ -25,6 +28,25 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = ("data_small/TG_STAID" + str(station).zfill(6) + ".txt")
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly_data(station, year):
+    filename = ("data_small/TG_STAID" + str(station).zfill(6) + ".txt")
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
